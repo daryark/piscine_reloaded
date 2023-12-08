189dda209fdb175ed484312c25fd77221398be48
Dasha Yarkovska <dyarkovs@c2s3d8.42wolfsburg.de>
2023-12-08 16:36:52 +0100

    piscine reloaded

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,13 +6,27 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 14:56:51 by dyarkovs          #+#    #+#             */
-/*   Updated: 2023/11/12 20:26:59 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2023/11/12 21:06:18 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-int	ft_strcmp(char *s1, char *s2)
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+	ft_putchar('\n');
+}
+
+void	ft_print_params(int argc, char **argv)
+{
+	while (*++argv)
+		ft_putstr(*argv);
+}
+
+int	ft_swap(char *s1, char *s2)
 {
 	while (*s1 || *s2)
 	{
@@ -33,12 +47,11 @@ int	main(int argc, char **argv)
 {
 	int		i;
 	char	*t;
-	char	*p_word;
 
 	i = 1;
 	while (argv[i + 1])
 	{
-		if (ft_strcmp(argv[i], argv[i + 1]))
+		if (ft_swap(argv[i], argv[i + 1]))
 		{
 			t = argv[i];
 			argv[i] = argv[i + 1];
@@ -47,12 +60,6 @@ int	main(int argc, char **argv)
 		}
 		i++;
 	}
-	while (*++argv)
-	{
-		p_word = *argv;
-		while (*p_word)
-			ft_putchar(*p_word++);
-		ft_putchar('\n');
-	}
+	ft_print_params(argc, argv);
 	return (0);
 }
